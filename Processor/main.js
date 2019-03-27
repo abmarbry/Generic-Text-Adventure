36e32839d0decca0ac9e94b367d9e65ab178dba2
Allison Marbry <abmarbry@gmail.com>
2019-03-27 17:14:38 -0500

    Successfully loaded JSON files.

--- Processor/main.js
+++ Processor/main.js
@@ -1,3 +1,34 @@
 $( document ).ready(function() {
-	
+	fetchJSON();
 });
+
+var fetchJSON = function() {
+	$.ajaxSetup({beforeSend: function(xhr){
+	if (xhr.overrideMimeType)
+	{
+		xhr.overrideMimeType("application/json");
+	}
+	}
+	});
+	
+	
+	$.getJSON(getFileName())
+    .done(function(json) {
+		loadFirstSnippet(json);
+    })
+    .fail(function(e) {
+		console.log(e);
+		
+    });
+};	
+
+var getFileName = function(){
+	//TO DO
+	return "Game_Content/Story/000.json";
+};
+
+var loadFirstSnippet = function(json){
+	var rawData = json;
+	
+	console.log(rawData);
+}
