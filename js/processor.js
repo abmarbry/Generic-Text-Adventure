e61defb226579205d0bd4cb37d8f9b84aed068b3
Allison Marbry <abmarbry@gmail.com>
2019-04-01 15:23:06 -0500

    Added addBreak() method and consequently global htmlStrings variable

--- js/processor.js
+++ js/processor.js
@@ -1,7 +1,9 @@
 //Line 2 or 4, which one is better?
 //var Processor = {};
 
-function Processor (){};
+function Processor (){
+	this.htmlStrings = [];
+};
 
 Processor.prototype.translate = function(json){
 	var body = json.body;
@@ -14,23 +16,38 @@ Processor.prototype.translate = function(json){
 		var fetcher = new WordFetcher(body[bodyPos]);
 		while(!fetcher.isEmpty()){
 			var word = fetcher.next();
-			console.log(word);
+			//TO DO: minimize json data sent
+			handleAndInsert(json, word);
 		}
+		addBreak();
 		
 		bodyPos++;
 	}
 	
 	/*
-		1) Fetch word in "body"
-		2) If end of String, make paragraph break
-			a) If no other Strings in the array, mark exit condition true.
-		3) Else if choice, process choice consequences
-		4) Else if variable, insert variable accordingly
-		5) Else, normal word, paste accordingly
-		6) Go to 1) until exit condition is met
+		X	1) Fetch word in "body"
+		X	2) If end of String, make paragraph break
+		X		a) If no other Strings in the array, mark exit condition true.
+			3) Else if choice, process choice consequences
+			4) Else if variable, insert variable accordingly
+			5) Else, normal word, paste accordingly
+			6) Go to 1) until exit condition is met
 	*/
- };
+};
 
+function handleAndInsert(json, word){
+	//TO DO
+}
+ 
+ 
+function isChoiceOrVariable(word){
+	//TO DO LATER: possible fix somewhere for splitting a word if there's a \t or something at the end
+	return (word.charAt(0) === '<' && word.charAt(word.length-1) === '>');
+}
+
+function addBreak(){
+	this.htmlStrings.push("</br>");
+}
 
 //WORD FETCHER
 function WordFetcher(string){
