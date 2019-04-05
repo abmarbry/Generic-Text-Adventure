2cc8e14370fbae17bf0fa54491fab9f78f2a1d50
Allison Marbry <abmarbry@gmail.com>
2019-04-05 08:51:47 -0500

    Fixed bug where every choice got last isOutside value.

--- js/snippet.js
+++ js/snippet.js
@@ -2,7 +2,7 @@ function Snippet(){
 	this.htmlStrings = [];
 	this.choiceNext = [];
 	this.choiceConsequences = [];
-	this.choiceIsOutside = {};
+	this.choiceIsOutside = [];
 	
 	this.addChoiceHtmlString = function(index, body){
 		var string = "<span class='choice' id='" + index + "'>" + body + "</span>";
@@ -13,7 +13,7 @@ function Snippet(){
 Snippet.prototype.addHtmlChoice = function(next, consequences, isOutside, body){
 	this.choiceNext.push(next);
 	this.choiceConsequences.push(consequences);
-	this.choiceIsOutside = isOutside;
+	this.choiceIsOutside.push(isOutside);
 	//TO DO LATER: Handle if somehow they don't have the same length
 	var index = this.choiceNext.length-1;
 	this.addChoiceHtmlString(index, body);
@@ -22,7 +22,7 @@ Snippet.prototype.addHtmlChoice = function(next, consequences, isOutside, body){
 Snippet.prototype.getChoiceParameters = function(index){
 	return {next: this.choiceNext[index],
 			consequences: this.choiceConsequences[index],
-			isOutside: this.choiceIsOutside};
+			isOutside: this.choiceIsOutside[index]};
 }
 
 Snippet.prototype.add = function(string){
